ee622a375eee988a959f5bdeeb2a2ed4fc26f75e
Mamgui <mamgui.pa@gmail.com>
2018-02-19 17:31:34 -0300

    Install webpack-dev-server, create Index's Solved #7 and #9

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement("div", null, "Hi Universe! by Zaiens"), // <div>Hello World</div>
+  document.getElementById("root")
+);
